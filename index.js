687fc883f2f23254e63262b8d1089e3c52ac69c3
william <williamwambugu663@gmail.com>
2025-03-06 11:23:58 +0300

    Arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+//Divide function expression
+function divide(num1, num2){
+    return num1/num2;
+}
+divide(2000, 100);
+//square arrow function
+const square = (num3) => num3*num3;
+//Adding two numbers using arrow functions
+const add = (num4, num5) => num4 + num5;
